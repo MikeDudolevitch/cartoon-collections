67f6e753502fa3bbb236118eab45218fc8ad1ba5
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-02-03 16:07:52 -0500

    commit tree 2a0d645c6701316fd6ae878aa3b521db2c3a0435

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(dwarves)
+      dwarves.each.with_index(1) do |dwarf, index|
+      puts "#{index}. #{dwarf}"
+    end
+  end
+ 
+def summon_captain_planet(elements)
+  elements.collect do |element|
+  element.capitalize + "!"
+  # binding.pry
+  end
 end
-
-def summon_captain_planet# code an argument here
-  # Your code here
+ 
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    call.length > 4
+  end
 end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+ 
+def find_the_cheese(string)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  string.detect do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
+
